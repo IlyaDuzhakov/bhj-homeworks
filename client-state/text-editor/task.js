84b36e309e3dc708d6c8dc03c9689c61f2cc3142
Ilya Duzhakov <duzhakovip@mail.ru>
2025-03-31 14:46:39 +0300

    delete if from text-editor

--- client-state/text-editor/task.js
+++ client-state/text-editor/task.js
@@ -1,15 +1,13 @@
-const textArea = document.querySelector('#editor')
-const btnClear = document.querySelector('.clear')
+const textArea = document.querySelector("#editor");
+const btnClear = document.querySelector(".clear");
 
-if (localStorage.getItem('text')) {
-    textArea.value = localStorage.getItem('text')
-}
+textArea.value = localStorage.getItem("text");
 
-textArea.addEventListener('input', () => {
-    localStorage.setItem('text', textArea.value)
-})
+textArea.addEventListener("input", () => {
+  localStorage.setItem("text", textArea.value);
+});
 
-btnClear.addEventListener('click', () => {
-    textArea.value = ''
-    localStorage.setItem('text', textArea.value)
-})
+btnClear.addEventListener("click", () => {
+  textArea.value = "";
+  localStorage.setItem("text", textArea.value);
+});
